287dd798dac85937791c7df3c6363608ceb54911
Guy Bridge <guy@wsit.com.au>
2016-07-21 17:26:58 +0800

    Added constructor and new Jar class

--- Game.java
+++ Game.java
@@ -1,7 +1,23 @@
-public class Game {
+public class Game
+{
  
-  public static void main(String[] args) {
-    // Your code here
-  }
+	public static void main(String[] args)
+	{
+
+		System.out.println("");
+		System.out.println("****************************");
+		System.out.println("****************************");
+		System.out.println("****************************");
+		System.out.println("|||  LET THE GAME BEGIN  |||");	
+		System.out.println("****************************");
+		System.out.println("****************************");
+		System.out.println("****************************");
+		System.out.println("");
+
+		// Create a jar object
+		Jar jar = new Jar(args[0]);
+		System.out.println("The type of item in the jar is: " + jar.getItemName());			
+		  
+	}
   
 }
---
+++ Jar.java
@@ -0,0 +1,17 @@
+public class Jar
+{
+	// Fields
+	private String mItem;
+
+	// Our constructor which we will init the type of items in the jar
+	public Jar(String item)
+	{
+		mItem = item;
+	}
+
+	public String getItemName()
+	{
+		return mItem;
+	}	
+
+}
